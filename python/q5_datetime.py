c423b5dbfc32fa1dd135038e48c9a55183815ff6
johnboland91 <johnboland91@gmail.com>
2017-01-27 16:04:40 -0600

    Update q5_datetime.py
    datetime complete

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,68 @@
 # Hint:  use Google to find python function
+import datetime as dt
+###a) 
+date_start = '01-02-2013'.split("-")
+year1 = int(date_start[2])
+month1 = int(date_start[0])
+day1 = int(date_start[1])
+date1 = dt.date(year1,month1,day1)
 
-####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+date_stop = '07-28-2015'.split("-")
+year2 = int(date_stop[2])
+month2 = int(date_stop[0])
+day2 = int(date_stop[1])
+date2 = dt.date(year2,month2,day2)
+
+print(date2 - date1)
 
 ####b)  
-date_start = '12312013'  
+import datetime as dt
+
+date_start = '12312013'
+year1 = int(date_start[4:])
+month1 = int(date_start[0:2])
+day1 = int(date_start[2:4])
+date1 = dt.date(year1,month1,day1)
+
 date_stop = '05282015'  
+year2 = int(date_stop[4:])
+month2 = int(date_stop[0:2])
+day2 = int(date_stop[2:4])
+date2 = dt.date(year2,month2,day2)
+
+print(date2 - date1)
 
 ####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+import calendar
+import datetime as dt
+
+months = {
+    "Jan": 1,
+    "Feb": 2,
+    "Mar": 3,
+    "Apr": 4,
+    "May": 5,
+    "Jun": 6,
+    "Jul": 7,
+    "Aug": 8,
+    "Sep": 9,
+    "Oct": 10,
+    "Nov": 11,
+    "Dec": 12
+}
+
+date_start = '15-Jan-1994'.split("-")
+year1 = int(date_start[2])
+month1 = months[date_start[1]]
+day1 = int(date_start[0])
+date1 = dt.date(year1,month1,day1)
+
+date_stop = '14-Jul-2015'.split("-")
+year2 = int(date_stop[2])
+month2 = months[date_stop[1]]
+day2 = int(date_stop[0])
+date2 = dt.date(year2,month2,day2)
+date2 = dt.date(year2,month2,day2)
+
+print(date2 - date1)
+ 
