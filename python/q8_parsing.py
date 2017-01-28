57e6dac75580eed37a63bfac9d27f5c7aff2b384
johnboland91 <johnboland91@gmail.com>
2017-01-27 20:30:56 -0600

    Update q8_parsing.py
    Parsing data return Aston-Villa

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -1,6 +1,11 @@
-# The football.csv file contains the results from the English Premier League. 
-# The columns labeled ‘Goals’ and ‘Goals Allowed’ contain the total number of 
-# goals scored for and against each team in that season (so Arsenal scored 79 goals 
-# against opponents, and had 36 goals scored against them). Write a program to read the file, 
-# then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
+import pandas as pd
+
+url = "https://raw.githubusercontent.com/johnboland91/dsp/master/python/football.csv"
+data = pd.read_csv(url)
+data["Net Goals"] = data["Goals"] - data["Goals Allowed"]
+data["Net Goals Difference"] = abs(data["Net Goals"])
+min_diff = min(data["Net Goals Difference"])
+min_diff_index = data.loc[data['Net Goals Difference'].idxmin()]
+print(min_diff_index["Team"])
+
 
